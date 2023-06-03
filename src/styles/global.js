0816b4b6b03355ca325b6bb3bb37dafb7d4fed4c
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-06-03 20:34:42 -0300

    first commit

---
+++ src/styles/global.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+    body{
+        margin: 0;
+        padding: 0;
+        font-family: 'Poppins', Sans-Serif;
+        background-color: #F2F2F2;
+    }
+`;
+
+export default Global;
