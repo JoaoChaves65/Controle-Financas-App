5b4a2d2147624f38aa093eee38ae4ba718c1f69b
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-06-06 12:32:46 -0300

    Estilização do component Form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import * as C from './styles';
+import * as C from './styles'
 
 const Form = () => {
     const [desc, setDesc] = useState("");
@@ -18,41 +18,41 @@ const Form = () => {
 
     return (
         <>
-            <div>
-                <div>
-                    <label>Descrição</label>
-                    <input
+            <C.Container>
+                <C.InputContent>
+                    <C.Label>Descrição</C.Label>
+                    <C.Input
                         value={desc}
                         onChange={(e) => setDesc(e.target.value)}
                     />
-                </div>
-                <div>
-                    <label>Valor</label>
-                    <input
+                </C.InputContent>
+                <C.InputContent>
+                    <C.Label>Valor</C.Label>
+                    <C.Input
                         value={amount}
                         type="number"
                         onChange={(e) => setDesc(e.target.value)}
                     />
-                </div>
-                <div>
-                    <input
+                </C.InputContent>
+                <C.RadioGroup>
+                    <C.Input
                         type="radio"
                         id="rIncome"
                         defaultChecked
                         name="group1"
                         onChange={() => setIsExpense(!isExpense)}
                     />
-                    <label htmlFor="rIncome">Entrada</label>
-                    <input
+                    <C.Label htmlFor="rIncome">Entrada</C.Label>
+                    <C.Input
                         type="radio"
                         id="rExpenses"
                         name="group1"
                         onChange={() => setIsExpense(!isExpense)}
                     />
-                    <label htmlFor="rExpenses">Saída</label>
-                </div>
-                <button onClick={handleSave}>Adicionar</button>
-            </div>
+                    <C.Label htmlFor="rExpenses">Saída</C.Label>
+                </C.RadioGroup>
+                <C.Button onClick={handleSave}>Adicionar</C.Button>
+            </C.Container>
         </>
     );
 };
